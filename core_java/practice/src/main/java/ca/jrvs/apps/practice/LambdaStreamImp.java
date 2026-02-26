343ed407dc9dc51342672b60b2701ba19c6f9d8f
Sumiksh Trehan <tsumiksh@gmail.com>
2026-02-26 18:14:09 +0000

    feat(streams): implemeted streams

---
+++ core_java/practice/src/main/java/ca/jrvs/apps/practice/LambdaStreamImp.java
@@ -0,0 +1,79 @@
+package ca.jrvs.apps.practice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class LambdaStreamImp implements LambdaStreamExc {
+
+  @Override
+  public Stream<String> createStrStream(String... strings) {
+    return Arrays.stream(strings);
+  }
+
+  @Override
+  public Stream<String> toUpperCase(String... strings) {
+    return createStrStream(strings).map(String::toUpperCase);
+  }
+
+  @Override
+  public Stream<String> filter(Stream<String> stringStream, String pattern) {
+    return stringStream.filter(s -> !s.contains(pattern));
+  }
+
+  @Override
+  public IntStream createIntStream(int[] arr) {
+    return Arrays.stream(arr);
+  }
+
+  @Override
+  public <E> List<E> toList(Stream<E> stream) {
+    return stream.collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Integer> toList(IntStream intStream) {
+    return intStream.boxed().collect(Collectors.toList());
+  }
+
+  @Override
+  public IntStream createIntStream(int start, int end) {
+    return IntStream.rangeClosed(start, end);
+  }
+
+  @Override
+  public DoubleStream squareRootIntStream(IntStream intStream) {
+    return intStream.asDoubleStream().map(Math::sqrt);
+  }
+
+  @Override
+  public IntStream getOdd(IntStream intStream) {
+    return intStream.filter(n -> n % 2 != 0);
+  }
+
+  @Override
+  public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
+    return msg -> System.out.println(prefix + msg + suffix);
+  }
+
+  @Override
+  public void printMessages(String[] messages, Consumer<String> printer) {
+    createStrStream(messages).forEach(printer);
+  }
+
+  @Override
+  public void printOdd(IntStream intStream, Consumer<String> printer) {
+    getOdd(intStream)
+        .mapToObj(String::valueOf)
+        .forEach(printer);
+  }
+
+  @Override
+  public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
+    return ints.flatMap(List::stream).map(n -> n * n);
+  }
+}
